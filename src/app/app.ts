f2eb38b554df056a778d09b70f81a1d165bc8a72
Suhaib Janjua <suhaib.janjua@gmail.com>
2025-09-07 03:00:45 +0500

    refactor(app): integrate atomic footer and migrate to standalone bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
